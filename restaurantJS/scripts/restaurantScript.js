1ae9f9fd69729945574d409f87050896e5f555d0
Kristina Zakharova <zakharova.kr@gmail.com>
2020-04-09 14:55:36 -0400

    finished form validation, added a modal, fixed small issues

--- restaurantJS/scripts/restaurantScript.js
+++ restaurantJS/scripts/restaurantScript.js
@@ -10,10 +10,48 @@
                 if (form.checkValidity() === false) {
                     event.preventDefault();
                     event.stopPropagation();
+                    form.classList.add('was-validated');
+                } else {
+                    event.preventDefault();
+                    event.stopPropagation();
+                    displayMessage();
+                    form.classList.remove("was-validated");
+                    clearMyFields();
+
                 }
-                form.classList.add('was-validated');
             }, false);
         });
     }, false);
+
 })();
 
+function displayMessage() {
+    //display a success message
+    document.getElementById("myModal").style.display = "block";
+}
+
+function hideModal() {
+    document.getElementById("myModal").style.display = "none";
+}
+
+function clearMyFields() {
+    document.getElementById('name').value = "";
+    document.getElementById('email').value = "";
+    document.getElementById('phoneNumber').value = "";
+    document.getElementById('inquery').value = "catering";
+    document.getElementById('info').value = "";
+    document.getElementById('info').value = "";
+    document.getElementById("visitedNo").checked = true;
+    document.getElementById("visitedYes").checked = false;
+    document.getElementById("M").checked = false;
+    document.getElementById("T").checked = false;
+    document.getElementById("W").checked = false;
+    document.getElementById("Th").checked = false;
+    document.getElementById("F").checked = false;
+}
+
+function resetVal() {
+    var form = document.getElementById("myForm");
+    form.classList.remove("was-validated");
+    document.getElementById("success-message").style.display = "none";
+}
